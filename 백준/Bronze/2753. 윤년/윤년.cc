725cc5f62a986258438a66fc10938d853cb255a5
eoyan <109939867+eoyan@users.noreply.github.com>
2026-03-19 12:24:09 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char **argv) {
+  int Y;
+  cin >> Y;
+
+
+  if (Y % 4 == 0 && (Y % 100 != 0 || Y % 400 == 0)) {
+    cout << 1;
+  }
+
+  else {
+    cout << 0;
+  }
+  
+}
